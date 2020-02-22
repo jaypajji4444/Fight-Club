1e6459840a52e78349c65320b2de6bf9b32da5f7
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2020-02-22 21:52:40 +0530

    old

--- config/key.js
+++ config/key.js
@@ -1,3 +1,3 @@
 module.exports={
-    connectionKey:"mongodb+srv://jaypajji4444:jaymehta@1234@cluster0-0inad.mongodb.net/test?retryWrites=true&w=majority"
+    connectionKey:"mongodb://localhost:27017/loc"
 }
