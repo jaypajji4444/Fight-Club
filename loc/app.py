981a2966886e792cd4fd5fccff085fd797a9bfa7
Deep Gandhi <46297564+deep1401@users.noreply.github.com>
2020-03-01 10:09:29 +0530

    Server shutdown endpoint for app1

--- loc/app.py
+++ loc/app.py
@@ -7,9 +7,11 @@ import rpy2.robjects as robjects
 from rpy2.robjects.packages import importr
 from twilio.rest import Client
 
-
-
-
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
@@ -21,6 +23,10 @@ df4=df3
 def hello_world():
     return 'Hello World!'
 
+@app.route('/shutdown', methods=['GET'])
+def shutdown():
+    shutdown_server()
+    return 'Server shutting down...'
 
 @app.route('/player_data',methods=['POST'])
 def player():
